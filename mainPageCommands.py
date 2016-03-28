81a420fa4182165af536d5c99140ee8e823542b5
mohammad .k <mkermani144@gmail.com>
2016-03-28 17:55:03 +0430

    Widgets of GetIP page added.

--- mainPageCommands.py
+++ mainPageCommands.py
@@ -3,4 +3,8 @@ from tkinter import ttk
 def openGetIP(root):
 	t=Toplevel(root)
 	t.title("ورود آی پی")
-	
+	ttk.Label(t,text=':لطفا آی پی سرور را وارد کنید').grid(row=0,column=0,padx=10,pady=10)
+	ttk.Entry(t,width=50).grid(row=1,column=0,padx=10,pady=10)
+	ttk.Button(t,text='تایید').grid(row=2,column=0,padx=10,pady=10)
+
+
